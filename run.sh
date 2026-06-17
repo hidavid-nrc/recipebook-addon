43026cbf19f1498d88f691c79764d5f06e007554
Hidavid <58142385+hidavid-nrc@users.noreply.github.com>
2026-06-17 16:55:58 +0200

    v0.5.2 - fix 413 on EPUB upload (ingress_stream)

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ export BRING_ENTITY=$(bashio::config 'bring_entity')
 export DATA_DIR="/share/recipebook"
 export BACKUP_DIR="/backup/recipebook"
 export PORT=8000
-VERSION="${APP_VERSION:-0.5.1}"
+VERSION="${APP_VERSION:-0.5.2}"
 
 mkdir -p "${DATA_DIR}" "${BACKUP_DIR}"
 
